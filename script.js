3065739c0a806469866a05263ac357935f0f7eb3
DGallagher12 <116509180+DGallagher12@users.noreply.github.com>
2022-11-15 16:30:56 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,47 @@
+const playa = document.querySelector("#playa");
+const comp = document.querySelector("#comp");
+const res = document.querySelector("#res");
+const buttons = document.querySelectorAll(".buttons");
+let hero;
+let villain;
+let result;
+
+buttons.forEach(button => button.addEventListener("click", () => {
+
+    hero = button.textContent;
+    randChoice();
+    playa.textContent = `Hero: ${hero}`;
+    comp.textContent = `Villain: ${villain}`;
+    res.textContent = whoWins();
+}));
+
+function randChoice(){
+
+    const randNum = Math.floor(Math.random() * 3) + 1;
+
+    switch(randNum){
+      case 1:
+        villain = "ROCK";
+        break;
+      case 2:
+        villain = "PAPER";
+        break;
+      case 3:
+        villain = "SCISSORS";
+        break;
+    }
+}
+function whoWins(){
+    if(hero == villain){
+      return "It's a draw! Refresh and let's settle this";
+    }
+    else if(villain == "ROCK"){
+      return (hero == "PAPER") ? "You Win! Refresh and play again!" : "You Lose! Refresh and try again."
+    }
+    else if(villain == "PAPER"){
+      return (hero == "SCISSORS") ? "You Win! Refresh and play again!" : "You Lose! Refresh and try again."
+    }
+    else if(villain == "SCISSORS"){
+      return (hero == "ROCK") ? "You Win! Refresh and play again!" : "You Lose! Refresh and try again."
+    }
+}
