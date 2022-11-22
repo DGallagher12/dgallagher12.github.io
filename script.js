fd52e1c604f863e719f13e3836bdfa23368184a9
DGallagher12 <116509180+DGallagher12@users.noreply.github.com>
2022-11-22 21:36:07 +0000

    Add files via upload

--- script.js
+++ script.js
@@ -1,47 +1,28 @@
-const playa = document.querySelector("#playa");
-const comp = document.querySelector("#comp");
-const res = document.querySelector("#res");
-const buttons = document.querySelectorAll(".buttons");
-let hero;
-let villain;
-let result;
-
-buttons.forEach(button => button.addEventListener("click", () => {
-
-    hero = button.textContent;
-    randChoice();
-    playa.textContent = `Hero: ${hero}`;
-    comp.textContent = `Villain: ${villain}`;
-    res.textContent = whoWins();
-}));
-
-function randChoice(){
-
-    const randNum = Math.floor(Math.random() * 3) + 1;
-
-    switch(randNum){
-      case 1:
-        villain = "ROCK";
-        break;
-      case 2:
-        villain = "PAPER";
-        break;
-      case 3:
-        villain = "SCISSORS";
-        break;
+function start(){
+var numb = 99,
+    bottle = "bottles",
+    lyrics = "",
+    output = document.getElementById('container');
+while (numb > 0) {
+    if (numb == 1) {
+        bottle = "bottle";
     }
+
+    lyrics += numb + " ";
+    lyrics += bottle + " of beer on the wall, ";
+    lyrics += numb + " " + bottle + " of beer.<br>";
+    
+    numb--;
+    lyrics += "Take one down and pass it around, ";
+    lyrics += + numb + " bottles of beer on the wall.<hr>"
+
+    if (numb == 0) {
+        numb = "no more";
+    }
+    output.innerHTML += lyrics;
+    lyrics = '';
 }
-function whoWins(){
-    if(hero == villain){
-      return "It's a draw! Choose again and let's settle this!";
-    }
-    else if(villain == "ROCK"){
-      return (hero == "PAPER") ? "You Win! Press another button to play again!" : "You Lose! Choose again to get payback!"
-    }
-    else if(villain == "PAPER"){
-      return (hero == "SCISSORS") ? "You Win! Press another button to play again!" : "You Lose! Choose again to get payback!"
-    }
-    else if(villain == "SCISSORS"){
-      return (hero == "ROCK") ? "You Win! Press another button to play again!" : "You Lose! Choose again to get payback!"
-    }
+
+output.innerHTML += " No more bottles of beer on the wall, no more bottles of beer.  Go to the store and buy some more, 99 bottles of beer on the wall.";
+
 }
